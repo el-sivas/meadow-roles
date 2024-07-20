801b863e9aec17cc2f094b3cc90a4fefc27a10d3
sl-168@gmx.net <sl-168@gmx.net>
2024-07-20 15:57:23 +0200

    to-print

--- to_print/src/to_print.sh
+++ to_print/src/to_print.sh
@@ -6,19 +6,27 @@ to_print_dir=/var/tmp/to_print
 printed_dir=/var/tmp/printed
 print_fails_dir=/var/tmp/print_fails
 
+function log_info() {
+    echo "$(date) [INFO] $1"
+}
+
+function log_error() {
+    echo "$(date) [ERROR] $1" >&2
+}
+
 if [ -z "$(find "$to_print_dir" -name '*.pdf')" ]; then
-    echo "No PDF files found in $to_print_dir"
+    log_info "No PDF files found in $to_print_dir"
     exit 0
 fi
 
 for file in "$to_print_dir"/*.pdf; do
-    echo -n "print '$file'..."
+    log_info -n "print '$file'..."
     if ! lp -d "$printer" "$file"
     then
         mv "$file" "$print_fails_dir"
-        echo "FAILED"
+        log_error "print '$file' failed, moved to '$print_fails_dir"
     else
         mv "$file" "$printed_dir"
-        echo "OK"
+        log_info "..OK"
     fi
 done
