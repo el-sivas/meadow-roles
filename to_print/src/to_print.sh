2d01d58700f80638ab7a0f26bfb438d8981549cd
sebastian <info.sl@gmx.net>
2024-07-20 14:44:46 +0200

    to-print

--- to_print/src/to_print.sh
+++ to_print/src/to_print.sh
@@ -1,4 +1,4 @@
-#!/src/bash
+#!/bin/bash
 
 printer="Kyocera_FS-1030D"
 
@@ -6,11 +6,19 @@ to_print_dir=/var/tmp/to_print
 printed_dir=/var/tmp/printed
 print_fails_dir=/var/tmp/print_fails
 
+if [ -z "$(find "$to_print_dir" -name '*.pdf')" ]; then
+    echo "No PDF files found in $to_print_dir"
+    exit 0
+fi
+
 for file in "$to_print_dir"/*.pdf; do
+    echo -n "print '$file'..."
     if ! lp -d "$printer" "$file"
     then
-        mv "$file" "$printed_dir"
-    else
         mv "$file" "$print_fails_dir"
+        echo "FAILED"
+    else
+        mv "$file" "$printed_dir"
+        echo "OK"
     fi
 done
